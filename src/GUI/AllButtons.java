e0d2fd5d6879f7f4d7fa572f0d14ed3e3f9d94f2
Niirok <anthony_youssef@hotmail.fr>
2015-05-13 18:50:24 +0200

    commit de reconstruction, WiP

---
+++ src/GUI/AllButtons.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.JPanel;
+
+public class AllButtons extends JPanel {
+    public AllButtons(){
+	MainFrame mainframe = new MainFrame();
+	GenerateButton generate = new GenerateButton("generate", mainframe);
+	//Si ajout de nouveau buton, le faire ici !!
+	//
+	JPanel Allbuttons = new JPanel();
+    }
+
+}
--- src/System/Main.java
+++ src/System/Main.java
@@ -9,7 +9,8 @@ public class Main {
 	String tabC[] = {"Albert","Bob"};
 	Film f1 = new Film("nom au pif", 1993,Tabg,tabC ,120, "le pape"," C'est l'histoire d'un mec");
 	System.out.println(f1.toString());*/
-	DataRead.ReadingFromData();
+	AbsVideoDatabase DB = new AbsVideoDatabase(); 
+	DataRead.ReadingFromData(DB);
     }
 
 }
