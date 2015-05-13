56664ebd071ece1e0cfc4dbe3672bf00fbaa619d
Anthony Youssef <Anthony_Youssef@hotmail.fr>
2015-05-13 21:01:58 +0200

    Final reconstruction !

---
+++ src/system/Main.java
@@ -0,0 +1,11 @@
+package system;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+	AbsVideoDatabase DB = new AbsVideoDatabase(); 
+	DataRead.ReadingFromData(DB);
+    }
+}
