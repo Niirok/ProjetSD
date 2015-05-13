ca72f178ce4477f79738106bd8cc87681abcbbe6
adetncy <adelaidearmand@gmail.com>
2015-05-13 15:23:25 +0200

    Revert "Revert " corrections mineures""
    This reverts commit 533a2705cc90b9550aedfe47f10694a3bd9c923b.

--- src/System/Film.java
+++ src/System/Film.java
@@ -2,18 +2,14 @@ package System;
 
 public class Film extends AbsVideo {
     static String type ="Film";
-    protected String director;
-
 
     public Film(String name, int year, String[] genre, String[] casting, int duration, String director, String synop) {
-	super(name, year, genre, casting, duration,synop);
-	this.director = director;
+	super(name, year, genre, casting, duration,synop, director);
+
     }
     
     public String toString(){
-	String toReturn = super.toString();
-	toReturn += "\nDirector : " + director;
-	return toReturn;
+	return super.toString();
     }
    public String getType(){
 	return Film.type;
